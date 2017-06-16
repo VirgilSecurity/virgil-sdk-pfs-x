b40d1ff223f2b0453e409e9465e7f11704fcc11c
Oleksandr Deundiak <Deundiak@gmail.com>
2017-06-16 14:48:12 +0300

    Added dummy tests

---
+++ Tests/VSSTestsConst.h
@@ -0,0 +1,24 @@
+//
+//  VSSTestsConst.h
+//  VirgilSDK
+//
+//  Created by Oleksandr Deundiak on 10/24/16.
+//  Copyright © 2016 VirgilSecurity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//In order to make this work, substitute appropriate values
+@interface VSSTestsConst : NSObject
+
+@property (nonatomic, readonly) NSString * __nonnull applicationToken;
+@property (nonatomic, readonly) NSString * __nonnull applicationPrivateKeyBase64;
+@property (nonatomic, readonly) NSString * __nonnull applicationPrivateKeyPassword;
+@property (nonatomic, readonly) NSString * __nonnull applicationIdentityType;
+@property (nonatomic, readonly) NSString * __nonnull applicationId;
+
+@property (nonatomic, readonly) NSURL * __nonnull cardsServiceURL;
+@property (nonatomic, readonly) NSURL * __nonnull cardsServiceROURL;
+@property (nonatomic, readonly) NSURL * __nonnull pfsServiceURL;
+
+@end
