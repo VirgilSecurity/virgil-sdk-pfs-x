a4b0400ed448778f6432f7987e8481054ca213f9
Oleksandr Deundiak <Deundiak@gmail.com>
2017-06-23 11:23:35 +0300

    Implemented EphemeralCardValidator

--- Source/Client/EphemeralCardValidator.swift
+++ Source/Client/EphemeralCardValidator.swift
@@ -7,3 +7,48 @@
 //
 
 import Foundation
+import VirgilSDK
+
+class EphemeralCardValidator {
+    let crypto: VSSCryptoProtocol
+    private var verifiers: [String: VSSPublicKey] = [:]
+    
+    static let ErrorDomain = "EphemeralCardValidatorErrorDomain"
+    
+    init(crypto: VSSCryptoProtocol) {
+        self.crypto = crypto
+    }
+    
+    
+    func addVerifier(withId verifierId: String, publicKeyData: Data) throws {
+        guard let publicKey = self.crypto.importPublicKey(from: publicKeyData) else {
+            throw NSError(domain: EphemeralCardValidator.ErrorDomain, code: -1, userInfo: [NSLocalizedDescriptionKey: "Error importing verifier public key."])
+        }
+        
+        self.verifiers[verifierId] = publicKey
+    }
+    
+    func validator(cardResponse: VSSCardResponse) -> Bool {
+        let fingerprint = self.crypto.calculateFingerprint(for: cardResponse.snapshot)
+        let cardId = fingerprint.hexValue
+        
+        guard cardId == cardResponse.identifier else {
+            return false
+        }
+        
+        for verifier in self.verifiers {
+            guard let signature = cardResponse.signatures[verifier.key], signature.count > 0 else {
+                return false
+            }
+            
+            do {
+                try self.crypto.verify(fingerprint.value, withSignature: signature, using: verifier.value)
+            }
+            catch {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
