4bf36aadab7f54198b8b543f6f7ebbdb1caf3f60
Oleksandr Deundiak <Deundiak@gmail.com>
2017-07-03 14:53:26 +0300

    Updated PFS error codes

--- Source/Client/Models/Errors/PFSError.swift
+++ Source/Client/Models/Errors/PFSError.swift
@@ -21,15 +21,17 @@ struct PFSError {
         let message: String
         
         switch code {
-        case .serverInternal:            message = "Internal server error. Please try again later."
-        case .accessToken:               message = "The Virgil access token or token header was not specified or is invalid"
-        case .cardNotAvailable:          message = "The Virgil Card is not available in this application"
-        case .invalidJson:               message = "JSON specified as a request is invalid"
-        case .invalidSnapshot:           message = "Request snapshot invalid"
-        case .signatureValidationFailed: message = "SCR sign validation failed (recipient)"
-        case .selfSignatureMissing:      message = "SCR sign item is invalid or missing for the Client (Self sign)"
-        case .globalCardScopeForbidden:  message = "Card scope should be application"
-        case .maximumOtcNumberExceeded:  message = "Maximum number of OTCs 100"
+        case .serverInternal:                 message = "Internal server error. Please try again later."
+        case .accessToken:                    message = "The Virgil access token or token header was not specified or is invalid"
+        case .cardNotAvailable:               message = "The Virgil Card is not available in this application"
+        case .invalidJson:                    message = "JSON specified as a request is invalid"
+        case .invalidSnapshot:                message = "Request snapshot invalid"
+        case .duplicateCardFingerprint:       message = "Virgil Card with the same fingerprint exists already"
+        case .signatureValidationFailed:      message = "SCR sign validation failed (recipient)"
+        case .globalCardScopeForbidden:       message = "Card scope should be application"
+        case .maximumOtcNumberExceeded:       message = "Maximum number of OTCs 100"
+        case .exceededNumberOfItemsInRequest: message = "Exceeded number of items in request"
+        case .unsupportedPublicKeyType:       message = "Unsupported public key type"
         }
         
         return message
@@ -67,15 +69,17 @@ extension PFSError: Deserializable {
 
 extension PFSError {
     enum ErrorCode: Int {
-        case serverInternal            = 10000
-        case accessToken               = 20300
-        case cardNotAvailable          = 20500
-        case invalidJson               = 30000
-        case invalidSnapshot           = 30001
-        case signatureValidationFailed = 30140
-        case selfSignatureMissing      = 30142
-        case globalCardScopeForbidden  = 60000
-        case maximumOtcNumberExceeded  = 60010
+        case serverInternal                 = 10000
+        case accessToken                    = 20300
+        case cardNotAvailable               = 20500
+        case invalidJson                    = 30000
+        case invalidSnapshot                = 30001
+        case duplicateCardFingerprint       = 30138
+        case signatureValidationFailed      = 30140
+        case globalCardScopeForbidden       = 60000
+        case maximumOtcNumberExceeded       = 60010
+        case exceededNumberOfItemsInRequest = 60011
+        case unsupportedPublicKeyType       = 60100
     }
 }
 
